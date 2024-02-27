f253cf67a9c6a4201e471dae817692308909aad9
bigban-ui <157366485+bigban-ui@users.noreply.github.com>
2024-02-27 15:48:34 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Purchase from "./pages/Purchase";
+import Connect from "./components/connect";
+import Deshbord from "./components/deshbord";
+import Saidbar from "./components/saidbar";
+import Sell from "./pages/sell";
+import Earning from "./pages/earning";
+import Referral from "./pages/referral";
+import Withdrawal from "./pages/withdrawal";
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <div className="container_main">
+      <Saidbar />
+      <Connect />
+      </div>
+      <Routes>
+        <Route path="/" element={<Deshbord />} />
+        <Route path="/purchase" element={<Purchase />} />
+        <Route path="/sell" element={<Sell />} />
+        <Route path="/earning" element={<Earning />} />
+        <Route path="/referral" element={<Referral />} />
+        <Route path="/withdrawal" element={<Withdrawal />} />
+
+      </Routes>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import Saidbar from './components/saidbar'; // Assuming your component file is named Saidbar
+import Connect from './components/connect';
+import Deshbord from './components/deshbord';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+} from "@thirdweb-dev/react";
 
+import { ChainId } from "@thirdweb-dev/sdk";
+
+// const chainId = ChainId.BinanceSmartChainMainnet;
+const chainId = ChainId.Polygon;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter >
+    <ThirdwebProvider
+      supportedWallets={[metamaskWallet(), coinbaseWallet(), walletConnect()]}
+      activeChain={137}
+
+      clientId="180e1ffa65b3ac39f89e2bef7ae2a850"
+    >
+        <App />
+      </ThirdwebProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const sidebarRoot = ReactDOM.createRoot(document.getElementById('saidbar'));
+// sidebarRoot.render(
+//   <React.StrictMode>
+//     <Saidbar />
+//   </React.StrictMode>
+// );
+
+// const deshbordRoot = ReactDOM.createRoot(document.getElementById('deshbord'));
+// deshbordRoot.render(
+//   <React.StrictMode>
+//     <Deshbord />
+//   </React.StrictMode>
+// );
+
+// const connectRoot = ReactDOM.createRoot(document.getElementById('connect'));
+// connectRoot.render(
+//   <React.StrictMode>
+//     <Connect />
+//   </React.StrictMode>
+// );
